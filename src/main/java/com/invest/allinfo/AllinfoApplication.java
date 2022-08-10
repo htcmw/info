acb1833abd70c6ee3ee8dcec649c265d1033cd19
htcmw <htcmw93@gmail.com>
2022-08-10 22:34:34 +0900

    프로젝트 생성

---
+++ src/main/java/com/invest/allinfo/AllinfoApplication.java
@@ -0,0 +1,13 @@
+package com.invest.allinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllinfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllinfoApplication.class, args);
+    }
+
+}
