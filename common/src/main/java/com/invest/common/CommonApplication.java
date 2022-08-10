cf57267662828bd2ebf1de80d17c83b2ee1ec41d
htcmw <htcmw93@gmail.com>
2022-08-10 23:35:53 +0900

    모듈 생성 및 build.gradle 설정

--- common/src/main/java/com/invest/common/CommonApplication.java
+++ common/src/main/java/com/invest/common/CommonApplication.java
@@ -1,13 +1,13 @@
-package com.invest.allinfo;
+package com.invest.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AllinfoApplication {
+public class CommonApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AllinfoApplication.class, args);
+        SpringApplication.run(CommonApplication.class, args);
     }
 
 }
